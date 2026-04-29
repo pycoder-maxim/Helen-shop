c1b78900d81ab2b2b4aa58bb65cb39d0d5c787bc
maksimmarkov <markovm2023@yandex.ru>
2026-04-29 16:50:40 +0300

    Add login/logout URLs

--- clothing_store/urls.py
+++ clothing_store/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
     path('auth/', include('tg_auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('shop.urls')),
 ]
 
